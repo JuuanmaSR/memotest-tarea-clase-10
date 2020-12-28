616a916994b50dcf6f8e7e61cb68a393b02c419c
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-12-28 15:23:21 -0300

    Creando configuracion de juego
    Seleccionando el tablero y los cuadros, cuando una funcion para asignarle los colores a los cuadros.

--- main/src/main.js
+++ main/src/main.js
@@ -1,1 +1,11 @@
-console.log(`hola mundo!`);
+let turnos = 0;
+let tiempo = 0;
+const $tablero = document.querySelector(`#tablero`);
+const $cuadros = $tablero.querySelectorAll(`.cuadro`);
+
+function configurarJuego() {
+    const coloresBase = ["verdeClaro", "rojo", "salmon", "azul", "amarillo", "naranja", "violeta", "verde"];
+    const coloresRepetidos = coloresBase.concat(coloresBase);
+    configurarCuadros($cuadros, coloresRepetidos);
+
+};
