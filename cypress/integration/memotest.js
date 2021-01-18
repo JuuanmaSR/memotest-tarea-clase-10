c5315eefb34d2a9a8e409b66fa7c43b57762da8e
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-01-18 16:19:50 -0300

    Primer test con cypress usando referencia de clase 11 memotest
    Se realizo un test con cypress para verificar la existencia de 16 cuadros en el tablero, el cual permanece oculto hasta apretar el boton comenzar.
    Ademas de eso se cambio la URL porque se habia modificado y daba error a la hora de testear

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -1,12 +1,16 @@
-const URLMEMOTEST = `192.168.1.2:8080`;
+const URLMEMOTEST = `http://192.168.1.4:8081`;
 
 context(`memotest`, () => {
     before(() => {
         cy.visit(URLMEMOTEST);
     });
 
-    it(`test`, ()=>{
-
+    describe(`juega al memotest`, () => {
+        const NUMEROS_CUADROS = 16;
+        it(`Asegurar la existencia de cuadros en el tablero`, () => {
+            cy.get(`#tablero`).find(`.cuadro`).should(`have.length`, NUMEROS_CUADROS);
+        });
     });
-    
+
+
 });
