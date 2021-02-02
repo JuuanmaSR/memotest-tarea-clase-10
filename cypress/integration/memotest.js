326d47e6aa08b53ba3b45e3608cb8c5d2a7d4a8f
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-02-02 15:23:15 -0300

    test-cypress
    Asegurando que el tablero se vuelva visible cuando se apreta el boton comenzar

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -1,4 +1,4 @@
-const URLMEMOTEST = `http://192.168.1.4:8081`;
+const URLMEMOTEST = `http://192.168.1.5:8080`;
 
 context(`memotest`, () => {
     before(() => {
@@ -8,10 +8,14 @@ context(`memotest`, () => {
     describe(`juega al memotest`, () => {
         const NUMEROS_CUADROS = 16;
         it(`Asegurar que el tablero no sea visible antes de apretar el boton comenzar`, () => {
-            cy.get(`#tablero`).should('not.visible');
+            cy.get(`#tablero`).should('not.be.visible');
         });
         it(`Asegurar la existencia de cuadros en el tablero`, () => {
             cy.get(`#tablero`).find(`.cuadro`).should(`have.length`, NUMEROS_CUADROS);
+        });
+        it('Asegurar que cuando se haga click en el boton comenzar se muestre el tablero', () => {
+            cy.get(`#comenzar`).click();
+            cy.get(`#tablero`).should('be.visible');
         });
     });
 
