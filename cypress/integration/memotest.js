a1466aa0b12cc1179340ce4c3383960c8f8864e1
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-02-02 16:11:40 -0300

    test-cypress
    Asegurar que los cuadros sean aleatorios para que el juego tenga sentido.

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -1,21 +1,45 @@
-const URLMEMOTEST = `http://192.168.1.5:8080`;
+const URLMEMOTEST = `http://192.168.1.5:8084`;
 
 context(`memotest`, () => {
     before(() => {
         cy.visit(URLMEMOTEST);
     });
 
-    describe(`juega al memotest`, () => {
+    describe(`iniciar memotest`, () => {
         const NUMEROS_CUADROS = 16;
         it(`Asegurar que el tablero no sea visible antes de apretar el boton comenzar`, () => {
             cy.get(`#tablero`).should('not.be.visible');
         });
+
         it(`Asegurar la existencia de cuadros en el tablero`, () => {
             cy.get(`#tablero`).find(`.cuadro`).should(`have.length`, NUMEROS_CUADROS);
         });
+
         it('Asegurar que cuando se haga click en el boton comenzar se muestre el tablero', () => {
             cy.get(`#comenzar`).click();
             cy.get(`#tablero`).should('be.visible');
+        });
+
+        it(`Asegurar que los cuadros sean aleatorios`, () => {
+            cy.get(`.cuadro`).then((cuadros) => {
+                let primerasClases = [];
+                cuadros.each(function (i, cuadro) {
+                    primerasClases.push(cuadro.className);
+                });
+
+                cy.visit(URLMEMOTEST);
+
+                let segudasClases = [];
+                cy.get(`.cuadro`).then((nuevosCuadros) => {
+                    nuevosCuadros.each(function (i, cuadro) {
+                        segudasClases.push(cuadro.className);
+                    });
+                    cy.wrap(primerasClases).should(`not.deep.equal`, segudasClases);
+                });
+            });
+        });
+        describe(`jugar al memotest`, () => {
+            
         });
     });
 
